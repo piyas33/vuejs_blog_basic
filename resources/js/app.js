f51e44e9e23e40756e3fcad0fa6e3627c1380052
piyas33 <safaetul.ahasan@gmail.com>
2021-03-01 22:44:37 +0600

    changes

---
+++ resources/js/app.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './components/App'
+import Hello from './components/Hello'
+import Home from './components/Home'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
